808dc7e4bbe688f2c2c7252bab8fae8fd2c5a430
phenicsl <phenicsl@gmail.com>
2010-08-09 01:38:26 +0800

    add .children and .content member

--- lib/Orgmode/OrgNode.pm
+++ lib/Orgmode/OrgNode.pm
@@ -5,8 +5,30 @@ use v6;
 class OrgNode {
     has Str $.title is rw;
     has Int $.level is rw;
+    has @.children is rw;
+    has Str $.content is rw;
 
-    method Str(){
-        "*" x $.level ~ " " ~ $.title;
+    method Str(){    
+        "OrgNode($.level, $.title)";
+    }
+
+    method perl() {
+        my Str $indent = ' ' x 4;
+        my $str = "OrgNode($.level, $.title)\{\n";
+	
+	if $.content {
+	    my $content = "$.content";
+	    $content ~~ s:g/^^/## /;
+	    $str ~= $content;
+        }
+
+	for @.children -> $child {
+	    my $cstr = $child.perl;
+	    $cstr ~~ s:g/^^/    /;
+	    $str ~= $cstr;
+	}
+	
+	$str ~= "\}\n";
+	$str;
     }
 }
--- t/01-orgnode.pl
+++ t/01-orgnode.pl
@@ -8,5 +8,7 @@ use Orgmode::OrgNode;
 
 
 my OrgNode $orgnode .= new(:title<The Title>, :level(1));
+my OrgNode $secondnode .= new(:title<Second title>, :level(2));
+$orgnode.children.push($secondnode);
 say $orgnode.perl;
 say $orgnode;
